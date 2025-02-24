946c7be63e1a00e020da877b6da37b8549206f81
telis-project <kalijunaidtest2@gmail.com>
2025-02-24 16:26:56 +0400

    Refactor namespaces and file structure for Filament resources and pages. Added the admin panel.

---
+++ app/Filament/App/Pages/EditTeam.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\App\Pages;
+
+use Filament\Facades\Filament;
+use Filament\Pages\Tenancy\EditTenantProfile;
+use Telis\CustomFields\Filament\Resources\CustomFieldResource;
+use Telis\CustomFields\Services\EntityTypeOptionsService;
+
+class EditTeam extends EditTenantProfile
+{
+    protected static string $view = 'filament.pages.edit-team';
+
+    protected static ?string $slug = 'team';
+
+    protected static ?int $navigationSort = 2;
+
+    public static function getLabel(): string
+    {
+        return 'Team Settings';
+    }
+
+    protected function getViewData(): array
+    {
+        return [
+            'team' => Filament::getTenant(),
+        ];
+    }
+}
