65dc8181d18aa93c8036ad9daf12c31a0f9db419
telis-project <kalijunaidtest2@gmail.com>
2025-05-02 15:50:38 +0400

    Refactor documentation layout and enhance card components with improved styling and smooth scrolling functionality

--- app-modules/Documentation/routes/web.php
+++ app-modules/Documentation/routes/web.php
@@ -6,6 +6,6 @@ use Illuminate\Support\Facades\Route;
 use Telis\Documentation\Http\Controllers\DocumentationController;
 
 Route::prefix('documentation')->name('documentation.')->group(function () {
-    Route::get('/', DocumentationController::class)->name('index');
-    Route::get('/{type}', DocumentationController::class)->name('show');
-}); 
+    Route::get('/', [DocumentationController::class, 'index'])->name('index');
+    Route::get('/{type}', [DocumentationController::class, 'show'])->name('show');
+});
