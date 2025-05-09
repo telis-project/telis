3bb9280a8e7c62dbce8002288adce6741c55c353
telis-project <kalijunaidtest2@gmail.com>
2025-05-09 16:03:52 +0400

    Refactor admin module: update resource paths and remove unnecessary overrides in ListTeams and TeamResource

--- app-modules/Admin/src/AdminPanelProvider.php
+++ app-modules/Admin/src/AdminPanelProvider.php
@@ -7,7 +7,6 @@ namespace Telis\Admin;
 use Awcodes\Overlook\OverlookPlugin;
 use Awcodes\Overlook\Widgets\OverlookWidget;
 use Filament\Http\Middleware\Authenticate;
-use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Navigation\NavigationGroup;
@@ -19,6 +18,7 @@ use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Telis\Admin\Filament\Pages\Dashboard;
@@ -33,14 +33,15 @@ final class AdminPanelProvider extends PanelProvider
         return $panel
             ->id('admin')
             ->domain('admin.'.parse_url((string) config('app.url'))['host'])
-            ->path('')
+            ->login()
+            ->spa()
             ->colors([
                 'primary' => Color::Amber,
             ])
             ->brandName('Telis Admin')
             ->favicon('images/logo.png')
-            ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'Telis\\Admin\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'Telis\\Admin\\Filament\\Pages')
+            ->discoverResources(in: app_path('Filament/App/Resources'), for: 'Telis\\Admin\\Filament\\Resources')
+            ->discoverPages(in: app_path('Filament/App/Pages'), for: 'Telis\\Admin\\Filament\\Pages')
             ->navigationGroups([
                 NavigationGroup::make()
                     ->label('User Management'),
@@ -58,7 +59,7 @@ final class AdminPanelProvider extends PanelProvider
             ->pages([
                 Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'Telis\\Admin\\Filament\\Widgets')
+            ->discoverWidgets(in: app_path('Filament/App/Widgets'), for: 'Telis\\Admin\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
                 OverlookWidget::class,
