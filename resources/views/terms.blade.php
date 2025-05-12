c282c072b83868e7b3e47c4bfb6d6ad0b1b2a07f
telis-project <kalijunaidtest2@gmail.com>
2025-05-12 22:42:36 +0400

    Enhance SEO and social sharing: add Open Graph meta tags and improve layout components

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,4 +1,8 @@
-<x-guest-layout :title="'Terms of Service - ' . config('app.name')">
+<x-guest-layout 
+    :title="'Terms of Service - ' . config('app.name')"
+    description="Telis Terms of Service - Use of the Telis website and all related services is subject to the following terms of service."
+    :ogTitle="'Terms of Service - ' . config('app.name')"
+    ogDescription="Read the Terms of Service for Telis. Learn about the rules and guidelines for using our open-source CRM platform.">
     <x-legal-document
         title="Terms of Service"
         subtitle="Use of the Telis website and all related services is subject to the following terms of service."
