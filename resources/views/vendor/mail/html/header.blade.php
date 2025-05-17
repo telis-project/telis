fe5a2d92503d09d4c6f547aca28b0fd2c86c4f8b
telis-project <kalijunaidtest2@gmail.com>
2025-05-17 23:34:01 +0400

    Add email template components: create button, header, footer, layout, message, panel, subcopy, and table blade files

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,8 @@
+@props(['url'])
+<tr>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('images/telis-logo.svg') }}" class="logo" alt="Telis Logo">
+        </a>
+    </td>
+</tr>
