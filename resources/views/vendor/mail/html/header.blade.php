c790226a29d0c3677824403e23cbbda80af246d5
telis-project <kalijunaidtest2@gmail.com>
2025-05-19 14:35:16 +0400

    Update logo dimensions and change logo image source in header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
     <td class="header">
         <a href="{{ $url }}" style="display: inline-block;">
-            <img src="{{ asset('images/telis-logo.svg') }}" class="logo" alt="Telis Logo">
+            <img src="{{ asset('images/telis-logo-white.png') }}" class="logo" alt="Telis Logo">
         </a>
     </td>
 </tr>
