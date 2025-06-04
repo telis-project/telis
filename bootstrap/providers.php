85b804a4b81a140ccecb2ac3dd3eb5b456e11e44
telis-project <kalijunaidtest2@gmail.com>
2025-06-04 21:03:02 +0400

    Refactor AdminPanelProvider to improve domain handling and update phpstan-baseline for cleaner error management

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,12 +3,12 @@
 declare(strict_types=1);
 
 return [
+    App\Providers\MacroServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     Telis\Admin\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
-    App\Providers\MacroServiceProvider::class,
     Telis\Documentation\DocumentationServiceProvider::class,
 ];
